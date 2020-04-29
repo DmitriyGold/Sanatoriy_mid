c0058be3a234f55931577c6d3fd6e933fb9416a1
Золотов Дмитрий Сергеевич <@DESKTOP-QA65QKN>
2020-04-29 07:16:03 +0600

    перед поездкой в анжерку

--- views/site/license.php
+++ views/site/license.php
@@ -9,12 +9,12 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="mt-5 pt-5 container  align-content-center">
     <div class="row  align-content-center">
         <div class="col-12  align-content-center">
-            
-            <img src="<?= Url::to('@web/images/license/license.jpg');?>" alt="лицензия"> <br><br>
+
+            <img src="<?= Url::to('@web/images/license/license.jpg'); ?>" alt="лицензия"> <br><br>
             <a href="#">открыть в формате .PDF</a>  
         </div>
     </div>
-    </div>
+</div>
 
- 
+
 
